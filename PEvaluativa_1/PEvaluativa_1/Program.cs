387d4e993adaf2d592e22df15e0c4ab6dd341b68
JoseCastro1800 <55769727+JoseCastro1800@users.noreply.github.com>
2019-11-26 22:17:34 -0800

    Update Program.cs

--- PEvaluativa_1/PEvaluativa_1/Program.cs
+++ PEvaluativa_1/PEvaluativa_1/Program.cs
@@ -10,6 +10,20 @@ namespace PEvaluativa_1
     {
         static void Main(string[] args)
         {
+
+            //Instanciamos la clase operacion
+            Operacion Op = new Operacion();
+            Menu();
+
+            //Creamos un metodo menu
+            void Menu()
+            {
+                //se le pide al usuario que ingrese el id de la persona y asi buscarlo
+                Console.WriteLine("Bienvenido a mi programa");
+                Console.WriteLine("Escribe la Id de la persona a buscar");
+                int n = Convert.ToInt32(Console.ReadLine());
+                Console.ReadKey();
+                Op.BuscarPersona(n);
         }
     }
 }
